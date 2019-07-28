687597e145b1af03c5fa2d9305c999aa592d7300
Vitor Mariano <vitorcor8@gmail.com>
2019-07-28 08:11:54 -0300

    Distancia feita

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
+from wtforms import Form
+from forms import DistanciaForm
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/distancia")
 def distancia():
-    return render_template("distancia.html")
+    form = DistanciaForm(request.form)
+    
+    if request.method == 'POST' and form.validate():
+        print ("POST request and form is valid")
+        features =  request.form['features']
+        metodo = request.form['metodo']
+        return render_template_string(completed_template, features=features, metodo=metodo)
+
+    else:
+
+        return render_template_string(template_form, form=form)
+
 
 @app.route("/oraculo")
 def oraculo():
